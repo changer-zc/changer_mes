6fbde9ef8e5e9072cf2e16cc1304bfe50e4de775
zcchanger <786928995@qq.com>
2021-05-16 11:46:13 +0800

    Initial commit

---
+++ mes/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.contrib import admin,messages
+from django.db import transaction
+from django.urls import reverse
+from .models import *
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin, ExportActionModelAdmin
+from import_export import resources
+import simpleui
+from mes import views
+# Register your models here.
+
+
+class Repair_Admin(admin.ModelAdmin):
+    #显示的字段
+    list_display = ('item_no','product_name','sn','board_sn','statu','be_overdue','receive_time','repair_time')
+    #每页展示的条数
+    list_per_page = 20
+    #点击保存并继续编辑这个按钮给取消掉
+    save_as_continue = False
+    #详情页面使用radio显示选项
+    radio_fields = {}
+    #列表页搜索字段
+    search_fields = ('item_no','sn','board_sn','repair_time',)
+    #在列表页可以编辑的字段
+    list_editable = ('statu','repair_time')
+    #列表页上时间搜索
+    date_hierarchy = 'repair_time'
+    #这个就牛逼了，下面这个函数，会让模型表可以正常添加，但是不能编制，添加后只能查看
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields+('item_no','product_name','sn','board_sn','statu','be_overdue','receive_time','repair_time')
+        return self.readonly_fields
+
+admin.site.register(Repair,Repair_Admin)
---
+++ mes/models.py
@@ -0,0 +1,23 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Repair(models.Model):
+    item_no = models.CharField(max_length=255, blank=True, null=True,verbose_name='料号')
+    product_name = models.CharField(max_length=255, blank=True, null=True,verbose_name='品名')
+    sn = models.CharField(max_length=255, blank=True, null=True,verbose_name='整机SN')
+    board_sn = models.CharField(max_length=255, blank=True, null=True,verbose_name='主板SN')
+    statu = models.IntegerField(blank=True, null=True,verbose_name='维修状态')
+    be_overdue = models.IntegerField(blank=True, null=True,verbose_name='是否过保')
+    receive_time = models.DateTimeField(blank=True, null=True,verbose_name='接收时间')
+    repair_time = models.DateTimeField(blank=True, null=True,verbose_name='修复时间')
+
+    class Meta:
+        managed = True
+        db_table = 'repair'
